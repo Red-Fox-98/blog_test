b6edc783e9084d1c626b38ef3404c17f7c968cd6
Евгения Зайцева <evgeniya.zaitseva.98@gmail.com>
2026-01-29 13:31:35 +0300

    Добавлена авторизация с помощью токена

--- src/Controller/Api/ApiLoginController.php
+++ src/Controller/Api/ApiLoginController.php
@@ -12,8 +12,13 @@ class ApiLoginController extends AbstractController {
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
     public function index(#[CurrentUser] ?User $user): Response
     {
+        if ($user === null) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => 'Welcome ' . $user->getEmail() . ' to your new controller!',
             'path' => 'src/Controller/ApiLoginController.php',
         ]);
     }
